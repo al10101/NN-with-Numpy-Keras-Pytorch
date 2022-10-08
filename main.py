28105e1645e36168abdb5bdc1e9c8660d01c49a1
Alberto Cabrera <albertocabja@gmail.com>
2022-10-08 02:57:41 -0500

    Updated the numpy approach

--- main.py
+++ main.py
@@ -1,10 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import numpy as np
+
+from numpy_approach import main as numpy_approach
+
+def from_labels_to_probabilities(y, m, l): 
+	'''
+	Function to process the different digits and return a probability for each digit
+	:param y: a vector containing 5000 labels, from 0 to 9
+	:param m: the number of data
+	:param l: the number of different labels
+	:return: a matrix containing probabilities
+	'''
+
+	# The column vector "y" contains a number for each data. To train a neural network that 
+	# predicts probabilites, we need the probabilites in the first place: when an element 
+	# has y = 2.0, the NN must know that the element has probabilites (0 1 0 0 0 0 0 0 0 0)
+	Y = np.zeros([m, l])
+	for i in range(m):
+		# -1 because python uses zero-indexing
+		Y[i, int(y[i]) - 1] = 1
+
+	# Fast check with a random label
+	ri = np.random.randint(0, m-1)
+	print('Labels changed to probabilities. Random check:')
+	print(f'Index {ri}: y= {y[ri]} ---> Y= {Y[ri]}')
+	print()
+
+	return Y
 
 def main():
 
-	pass
+	# Read the training data from the tools folder. The data is already prepared and
+	# stored as numpy files to facilitate the reading process. The X variable contains 5000
+	# data with 400 features each (20x20 pixels from each image). The y variable contains
+	# the labels of all the 5000 data.
+	X = np.load( os.path.join('tools', 'X.npy') )
+	y = np.load( os.path.join('tools', 'y.npy') )
+	print(f'X is a {X.shape} matrix')
+	print(f'y is a {y.shape[0]} - dimensional vector ')
+	print()
+
+	# All neural networks from the three approaches share the same characteristics:
+	# - 400 inputs
+	# - 1 hidden layer with 25 neurons
+	# - 10 outputs (1 for each possible digit)
+	# - regularization parameter=0.01
+	# - iterations (epochs) = 20
+	# So it is convenient to set all this info in a dictionary to be used in each code
+	hyperparams = {
+		'inputs': 400,
+		'hidden_layers': 1,
+		'neurons': 25,
+		'outputs': 10,
+		'lambda': 0.01,
+		'iters': 20,
+		'n_data': 5000
+	}
+
+	# Transform y to Y (labels to probabilites)
+	Y = from_labels_to_probabilities(y, hyperparams['n_data'], hyperparams['outputs'])
+
+	# ====================================================
+	# NUMPY APPROACH
+	# ====================================================
+	numpy_approach(X, y, hyperparams)
+
 
 if __name__ == '__main__':
 
--- numpy_approach.py
+++ numpy_approach.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 
-def read
+def main(X, y, hyperparameters):
+	'''
+	Function that contains routines to train a NN using only numpy
+	:param X: matrix containing 5000 data with 400 features each
+	:param y: vector containing the labels for the 5000 data
+	:hyperparameters: dictionary to describe the characteristics of the NN
+	'''
+
+	# First of all, initialize the matrices that will contain the weights of 
+	# each neuron:
+	# - theta1: from input layer to hidden layer
+	# - theta2: from hidden layer to output layer
+
+	# The initial weights can be generated randomly, but preferably with a
+	# random generator seed to obtain reproducible results
+	np.random.seed(100)
+	
+	# There is an extra column in each matrix to account for the bias value
+	theta1 = np.random.rand(hyperparameters['neurons'], hyperparameters['inputs'] + 1)
+	theta2 = np.random.rand(hyperparameters['outputs'], hyperparameters['neurons'] + 1)
+
+	# It is recommended to fix the values to a small range, so we use epsilon. epsilon depends
+	# on the dimensions of each matrix
+	epsilon = lambda x, y: 2.449489 / np.sqrt(x + y)
+	e1 = epsilon( theta1.shape[0], theta1.shape[1] )
+	e2 = epsilon( theta2.shape[0], theta2.shape[1] )
+
+	theta1 = theta1 * 2.0 * e1 - e1
+	theta2 = theta2 * 2.0 * e2 - e2
+
+
+
